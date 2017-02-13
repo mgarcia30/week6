917a5e36fd5f9070c7ac8b6c44c55c6fd111ace4
mgarcia30 <m-garcia@kellogg.northwestern.edu>
2017-02-12 23:37:48 -0600

    week6

---
+++ src/index.js
@@ -0,0 +1,64 @@
+
+// DO NOT MODIFY
+
+// So we can use React
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+// Helper functions
+// .shuffle() shuffles (randomizes the order of) an array
+// e.g. [1,2,3,4,5].shuffle()
+// getDeck() gives us an array of cards, represented as strings
+// Hint: JavaScript's built-in .splice() function will remove elements from an
+// array to create a new array. For example, a "hand" from a deck of cards might
+// go something like this:
+//
+// deck.splice(0,5)
+var helpers = require('./components/Helpers')
+var getDeck = helpers.getDeck
+Array.prototype.shuffle = helpers.shuffle
+
+// Our component classes
+var Card = require('./components/Card')
+
+// END OF STUFF TO NOT MODIFY
+
+
+var App = React.createClass({
+
+  dealmein: function() {
+    this.setState({
+      hand:getDeck().shuffle().splice(0,5)
+    })
+
+  },
+
+  getInitialState: function() {
+      return {
+        hand: ["face_down","face_down","face_down","face_down","face_down"]
+      }
+    },
+
+  //Creating of state above
+  render: function() {
+    return (
+      <div>
+        <h1>Welcome to the KIEI-924 Casino!</h1>
+
+          <div className="details col-sm-4">
+                      <h3><a href="#" className="btn btn-success" onClick={this.dealmein}>Deal Me In Big Guy!</a></h3>
+          </div>
+
+        <div className="row col-sm-12">
+          <Card card={this.state.hand[0]}/>
+          <Card card={this.state.hand[1]}/>
+          <Card card={this.state.hand[2]}/>
+          <Card card={this.state.hand[3]}/>
+          <Card card={this.state.hand[4]}/>
+        </div>
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(<App />, document.getElementById("app"))
